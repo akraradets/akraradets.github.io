80283e21e16a3ed29b1bc24c8df933563059c2d8
akraradets <akraradets@gmail.com>
2023-12-22 18:36:54 +0700

    update site

--- project/src/main.js
+++ project/src/main.js
@@ -1,6 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import './registerServiceWorker'
-import router from './router'
+import "../node_modules/bulma/css/bulma.min.css"
 
-createApp(App).use(router).mount('#app')
+createApp(App).mount('#app')
